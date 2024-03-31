352073da295eacb45fb9375566e5262d7c96b856
Cylindric <mark@hanfordonline.co.uk>
2024-03-31 13:51:07 +0100

    Tidying up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # stationeers_compass
 A Stationeers Compass
+
+```bash
+export STATIONEERS_PATH="/mnt/c/Users/Mark/Documents/My Games/Stationeers/saves"
+export STATIONEERS_SAVE="2024-03-27 Moon Normal"
+python3 compass.py
+```
--- compass.py
+++ compass.py
@@ -1,59 +1,54 @@
+import click
 import logging
 import os
 import time
 import xmltodict
-from compass.compass import Compass
+
+from stationeers.compass import Compass
+from stationeers.save import Save
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-save_path="/mnt/c/Users/Mark/Documents/My Games/Stationeers/saves"
-save_name="2024-03-27 Moon Normal"
-world_file=f"{save_path}/{save_name}/world.xml"
-world_mtime=0
+@click.command()
+@click.option('--path', envvar='STATIONEERS_PATH', help='Path to the Stationeers save files')
+@click.option('--save', envvar='STATIONEERS_SAVE', help="The name of the save")
+def run(path, save):
+    s = Save(f"{path}/{save}")
+    s.load()
 
-meta_file=f"{save_path}/{save_name}/world_meta.xml"
-file=open(meta_file, "r")
-meta_xml=file.read()
-file.close()
+    world_mtime=0
 
-# Find out what world we're on
-d=xmltodict.parse(meta_xml)
-world_name=d["WorldMetaData"]["WorldName"]
-print(f"World is {world_name}.")
-compass = Compass(world_name)
+    # Find out what world we're on
+    print(f"World is {s.world}.")
+    compass = Compass(s.world)
 
-# Find out where we're going
-target = [0, 0]
-print(f"Destination is [{target[0]:.0f},{target[1]:.0f}].\n")
+    # Find out where we're going
+    target = [0, 0]
+    print(f"Destination is [{target[0]:.0f},{target[1]:.0f}].\n")
 
-while True:
-    world_stat = os.stat(world_file)
-    if (world_stat.st_mtime > world_mtime):
-        world_mtime = world_stat.st_mtime
+    while True:
+        world_stat = os.stat(s.world_file)
+        if (world_stat.st_mtime > world_mtime):
+            world_mtime = world_stat.st_mtime
 
-        file=open(world_file, "r")
-        world_xml=file.read()
-        file.close()
+            for brain in s.get_brains():
+                name=brain["CustomName"]
+                name = name.replace("'s Brain's Brain", "")
+                x = float(brain["WorldPosition"]["x"])
+                y = float(brain["WorldPosition"]["y"])
+                z = float(brain["WorldPosition"]["z"])
+                position = [x, z]
 
-        d=xmltodict.parse(world_xml)
-        world_data=d["WorldData"]
-        all_things=world_data["AllThings"]
-        data=all_things["ThingSaveData"]
-        brains=[t for t in data if t.get('PrefabName', None) == 'OrganBrain']
+                distance = compass.distance([x, z], target)
+                bearing = compass.bearing(position, target)
 
-        for brain in brains:
-            name=brain["CustomName"]
-            name = name.replace("'s Brain's Brain", "")
-            x = float(brain["WorldPosition"]["x"])
-            y = float(brain["WorldPosition"]["y"])
-            z = float(brain["WorldPosition"]["z"])
-            position = [x, z]
+                print(f"{name} is at [{x:.0f},{z:.0f}]. Destination is {distance:.0f}m away at {bearing:.0f}°")
 
-            distance = compass.distance([x, z], target)
-            bearing = compass.bearing(position, target)
+            time.sleep(1)
+            
+        
+if __name__ == '__main__':
+    run()
 
-            print(f"{name} is at [{x:.0f},{z:.0f}]. Destination is {distance:.0f}m away at {bearing:.0f}°")
-
-        time.sleep(1)
